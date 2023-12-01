8652a9ad5a7144ea5a2aadb9877f73a326dfa41e
midlj <144878496+midlj@users.noreply.github.com>
2023-12-02 00:35:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var s=0;
+var m=24;
+var h=12;
+var time="PM";
+
+
+var print=document.getElementById("time");
+setInterval(()=>{
+    s+=1;
+     if(s > 59){
+         s=0;
+         m+=1;
+         if(m > 59){
+             m=0;
+             h+=1;
+             if(h>12)
+             {
+                 h=1;
+             }
+             
+         }
+     }
+     print.innerHTML=(h+':'+m+':'+s);
+ 
+ },1000);
